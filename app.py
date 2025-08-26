a41d2ced2b087390bfd1ffe287f6d516951ef298
hasithaPerera2002 <hasithaperera2017@gmail.com>
2025-08-26 22:54:24 +0530

    Add CORS middleware to FastAPI app and improve error handling in prediction function

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from typing import Literal
 import numpy as np
 import pandas as pd
 import joblib
+from fastapi.middleware.cors import CORSMiddleware
 
 # Load model and preprocessors
 scaler = joblib.load("scaler/scaler.pkl")
@@ -11,6 +12,13 @@ imputer = joblib.load("imputer/imputer.pkl")
 feature_columns = joblib.load("feature_columns/feature_columns.pkl")
 
 app = FastAPI(title="Churn Predictor")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # adjust to your React dev server URL
+    allow_credentials=True,
+    allow_methods=["*"],   # allows POST, GET, OPTIONS, etc.
+    allow_headers=["*"],   # allows Content-Type, Authorization, etc.
+)
 
 class UserInput(BaseModel):
     CreditScore: float
@@ -33,33 +41,36 @@ def predict(input: UserInput,model_name: Literal[
 
     try:
         model = joblib.load(f"models/model_{model_name}.pkl")
-        imputer = joblib.load("imputer.pkl")
-        scaler = joblib.load("scaler.pkl")
-        feature_columns = joblib.load("feature_columns.pkl")
+        imputer = joblib.load("imputer/imputer.pkl")
+        scaler = joblib.load("scaler/scaler.pkl")
+        feature_columns = joblib.load("feature_columns/feature_columns.pkl")
     except FileNotFoundError as e:
+        print(f"Error loading model or preprocessors: {e}")
         return {"error": str(e)}
+    
+    print(f"Using model: {model_name}")
+    print(f"User Input",input)
 
-    x = [input.data.get(col, 0) for col in feature_columns]
+    x = [input.dict().get(col, 0) for col in feature_columns]
     x = np.array([x])
 
     x_imputed = imputer.transform(x)
     x_scaled = scaler.transform(x_imputed)
 
     prediction = model.predict(x_scaled)[0]
+    print(f"Prediction: {prediction}")
     return {"Exited": bool(prediction)}
 
 @app.get("/metrics")
 def get_metrics():
-    models = ["decision_tree", "rf", "svc", "mlp", "ann", "logistic", "nn"]
+    try:
+        df = pd.read_csv("scores/model_scores_ann.csv")
+        metrics = dict(zip(df["Metric"], df["Score"]))
+        return {"Model Evaluation Scores": metrics}
+    except Exception as e:
+        return {"error": str(e)}
 
-    all_metrics = {}
 
-    for model_name in models:
-        try:
-            df = pd.read_csv(f"scores/model_scores_{model_name}.csv")
-            metrics = dict(zip(df["Metric"], df["Score"]))
-            all_metrics[model_name] = metrics
-        except Exception as e:
-            all_metrics[model_name] = {"error": str(e)}
-
-    return {"model_metrics": all_metrics}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
